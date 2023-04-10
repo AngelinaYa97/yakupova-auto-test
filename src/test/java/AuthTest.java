844d69631a83894defd04e5c724a0e01098e09e1
AngelinaKE97 <a.yakupova.kazanexpress@gmail.com>
2023-04-10 21:27:43 +0300

    Add changes2

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,42 +1,56 @@
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static io.qameta.allure.SeverityLevel.CRITICAL;
 
 public class AuthTest {
+
+    @BeforeEach
+    public void before() {
+        open("https://github.com/");
+        $(".h0-mktg").shouldBe(visible);
+    }
     @Test
     @Severity(CRITICAL)
     @Owner("AngelinaYa97")
     public void shouldAuthorizeTest() {
-        step("Открыть страницу https://github.com/", () -> {
-            open("https://github.com/");
-            $(".h0-mktg").shouldBe(Condition.visible);
+
+        step("Перейти в модалку авторизации пользователя", () -> {
+            $("[href='/login']")
+                    .click();
+            $("[id=login]")
+                    .shouldBe(visible);
         });
-        step("Кликнуть на кнопку Sign in", () -> {
-            $("[href='/login']").click();
-            $("[id=login]").shouldBe(Condition.visible);
+
+        step("Ввести данные в инпуты логина и пароля", () -> {
+            $("[id='login_field']")
+                    .sendKeys("AngelinaYa97");
+            $("[id='password']")
+                    .sendKeys("ang43");
         });
-        step("Заполнить инпуты логина и пароля", () -> {
-            $("[id='login_field']").sendKeys("AngelinaYa97");
-            $("[id='password']").sendKeys("ang43");
-        });
-        step("Кликнуть на кнопку Sign in", () -> {
-            $(".js-sign-in-button").click();
-            $("[id='global-nav']").shouldBe(Condition.visible);
-        });
-        step("Кликнуть по миниатюре аватарки", () -> {
-            $("[aria-label='View profile and more']").click();
+
+        step("Перейти в личный кабинет гитхаба", () -> {
+            $(".js-sign-in-button")
+                    .click();
+            $("[id='global-nav']")
+                    .shouldBe(visible);
+            $("[aria-label='View profile and more']")
+                    .click();
             $("[href='/AngelinaYa97?tab=repositories']")
-                    .shouldBe(Condition.visible);
+                    .shouldBe(visible);
         });
-        step("Кликнуть по Your profile", () -> {
-            $(byText("Your profile")).click();
-            $(".p-name").shouldBe(Condition.visible);
+
+        step("Перейти на страницу своего профиля в гитхабе", () -> {
+            $(byText("Your profile"))
+                    .click();
+            $(".p-name")
+                    .shouldBe(visible);
         });
     }
 }
